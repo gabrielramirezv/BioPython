0f02fa77145d93a9b738e96ada4a01e8cf598662
gabrielramirezv <gramirez@lcg.unam.mx>
2024-08-06 23:53:30 -0600

    Code is added to access to the FieldList of PubMed

--- src/Einfo_FieldList.py
+++ src/Einfo_FieldList.py
@@ -3,7 +3,7 @@ NAME
     Einfo
 
 VERSION
-    0.1
+    1.0
 
 AUTHOR
     Gabriel Ramirez Vilchis
@@ -29,11 +29,23 @@ SEE ALSO
 '''
 
 # Import libraries
+from Bio import Entrez
 
 # Register an email account
+Entrez.email = "gramirez@lcg.unam.mx"
 
 # Use Einfo and read the content
+handle = Entrez.einfo(db = "pubmed")
+record = Entrez.read(handle)
+handle.close()
 
 # Access to FieldList
+fields = record["DbInfo"]["FieldList"]
 
 # Print the content
+print("NAME\tFULL NAME\tDESCRIPTION")
+for field in fields:
+    name = field["Name"]
+    full_name = field["FullName"]
+    description = field["Description"]
+    print(f"{name}\t{full_name}\t{description}")
