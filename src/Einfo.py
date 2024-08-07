d4eb6609a2b75d448fab27890b41f9b29526fd31
gabrielramirezv <gramirez@lcg.unam.mx>
2024-08-06 20:07:45 -0600

    Se agrega algoritmo inicial

---
+++ src/Einfo.py
@@ -0,0 +1,37 @@
+'''
+NAME
+    amino_acid_content
+
+VERSION
+    0.1
+
+AUTHOR
+    Gabriel Ramirez Vilchis
+
+GITHUB
+    https://github.com/gabrielramirezv/Programacion_en_Python/blob/master/src/amino_acid_content.py
+
+DESCRIPTION
+    Provides more information about the data bases
+
+CATEGORY
+    Entrez
+
+USAGE
+    python Einfo.py
+
+ARGUMENTS
+    None
+
+SEE ALSO
+    AT_GC_percentage
+
+'''
+
+# Import libraries
+
+# Register an email account
+
+# Use Einfo and read the content
+
+# Print
