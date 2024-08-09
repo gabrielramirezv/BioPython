909404d42fd1bba18c38c4d5e32dfaecd9076000
gabrielramirezv <gramirez@lcg.unam.mx>
2024-08-08 18:45:42 -0600

    Algorithm to get info from the protein database is added

---
+++ src/protein_einfo.py
@@ -0,0 +1,43 @@
+'''
+NAME
+    protein_einfo
+
+VERSION
+    0.1
+
+TYPE
+    Exercise
+
+AUTHOR
+    Gabriel Ramirez Vilchis
+
+GITHUB
+    https://github.com/gabrielramirezv/BioPython/blob/master/src/protein_einfo.py
+
+DESCRIPTION
+    Gets the description of the field "ECNO" and the link 
+    "protein_protein_small_genome" from the protein database
+
+CATEGORY
+    Entrez
+
+USAGE
+    python src/protein_einfo.py
+
+ARGUMENTS
+    None
+
+SEE ALSO
+    Einfo
+
+'''
+
+# Import libraries
+
+# Register an email account
+
+# Get the information from the database
+
+# Get the description of ECNO and protein_protein_small_genome
+
+# Print results
