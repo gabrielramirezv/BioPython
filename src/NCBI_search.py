c22296f0a2637563af318c3bb3a8c132f7953db9
gabrielramirezv <gramirez@lcg.unam.mx>
2024-08-08 19:26:31 -0600

    Algorithm to search in NCBI is added

---
+++ src/NCBI_search.py
@@ -0,0 +1,53 @@
+'''
+NAME
+    NCBI_search
+
+VERSION
+    0.1
+
+TYPE
+    Exercise
+
+AUTHOR
+    Gabriel Ramirez Vilchis
+
+GITHUB
+    https://github.com/gabrielramirezv/BioPython/blob/master/src/NCBI_search.py
+
+DESCRIPTION
+    Search in a database from NCBI according to the author and terms 
+    in the title
+
+CATEGORY
+    Entrez
+
+USAGE
+    python src/NCBI_search.py
+
+ARGUMENTS
+    
+
+SEE ALSO
+    Esearch
+
+'''
+
+# Import libraries
+
+# Define errors
+
+# Register an email account
+
+# Create parser
+
+# Store the arguments
+
+# Register the database and the terms
+
+# Verify that the database exists
+
+# Search the terms in the specified database
+
+# Save the IDs in a new file
+
+# Let the user know that the search is finished
