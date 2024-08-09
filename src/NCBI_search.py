db284554e85cdf805e8604d54fa2d3376e856b05
gabrielramirezv <gramirez@lcg.unam.mx>
2024-08-08 22:10:42 -0600

    Documentation is completed

--- src/NCBI_search.py
+++ src/NCBI_search.py
@@ -22,10 +22,19 @@ CATEGORY
     Entrez
 
 USAGE
-    python src/NCBI_search.py
+    python src/NCBI_search.py [-h] [-db DATABASE] 
+    [-a AUTHOR] [-t TITLE WORDS SEPARATED BY COMMAS] [-o OUTPUT]
 
 ARGUMENTS
-    
+    -h, --help              show this help message and exit
+    -db DATABASE, --database DATABASE
+                            DataBase that you want to access to
+    -a AUTHOR, --author AUTHOR
+                            Term to search in the field "AUTHOR"
+    -t TITLE, --title TITLE
+                            Terms to search in the title separated by commas
+    -o OUTPUT, --output OUTPUT
+                            Name of the output file
 
 SEE ALSO
     Esearch
@@ -69,7 +78,7 @@ parser.add_argument("-o", "--output",
                     help="Name of the output file",
                     type=str,
                     required=False,
-                    default="results/esearch_results.txt")
+                    default="esearch_results.txt")
 
 # Store the arguments
 args = parser.parse_args()
@@ -80,11 +89,12 @@ title = args.title
 author = args.author
 filename = "results/" + args.output
 
-# Verify that there is enough information to search
+# Verify that there is at least a word to search
 try:
     if not title and not author:
         raise InvalidInputError("\nThere is not information to search.")
 
+# If there are not words to search, ask the user for some
 except InvalidInputError as invalid_input_error:
     print(invalid_input_error.args[0] 
           + " Please, enter at least an author or " 
@@ -94,14 +104,18 @@ else:
 
     # Verify that the database exists
     try:
+        # Get information about the available databases
         handle = Entrez.einfo()
         record = Entrez.read(handle)
         handle.close()
         database_is_valid = False
+        # Search the database in the existing databases list
         for database in record["DbList"]:
                 if data_base == database:
                     database_is_valid = True
                     break
+        # If the database does not exist, give to the user a list of
+        # the existing databases
         if database_is_valid == False:
             raise InvalidInputError("\nDataBase is not valid.")
         
@@ -113,7 +127,8 @@ else:
 
     else:
 
-        # Search the terms in the specified database
+        # Create the term to search, using the title terms, the author,
+        # or both if provided by the user
         if title and author:
             terms_in_title = title.replace(",", "[Title] OR ")
             terms_in_title += "[Title]"
@@ -125,13 +140,16 @@ else:
         else:
             term_to_search = author
 
-        # Save the IDs in a new file
-        with Entrez.esearch(db = data_base, term = term_to_search) as handle:
+        # Search the term in the database and get the information
+        with Entrez.esearch(db = data_base, 
+                            term = term_to_search) as handle:
             record = Entrez.read(handle)
 
+        # Create the file and write the IDs in it
         with open(filename, "w") as file:
             for id in record["IdList"]:
                 file.write(f"{id}\n")
 
-        # Let the user know that the search is finished
+        # Let the user know that the search is finished and where
+        # the results are
         print(f"\nThe results are now available in {filename}\n")
