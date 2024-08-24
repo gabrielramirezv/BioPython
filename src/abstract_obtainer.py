9d16fe9e21fae14bbe9e89324e6f793b179acd70
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2024-08-24 00:31:55 -0600

    Algorithm to get the abstracts of articles from PubMed is created

---
+++ src/abstract_obtainer.py
@@ -0,0 +1,59 @@
+'''
+NAME
+    abstract_obtainer
+
+VERSION
+    0.1
+
+AUTHOR
+    Gabriel Ramirez Vilchis
+
+GITHUB
+    https://github.com/gabrielramirezv/BioPython/blob/master/src/abstract_obtainer.py
+
+DESCRIPTION
+    Receives a file with IDs of articles of interest and creates a file
+    with the abstracts of those articles, including IDs of other 
+    papers that cited them.
+
+CATEGORY
+    Entrez
+    
+REQUIREMENTS
+    - Software - 
+        You must have Python and BioPython installed
+    - Repository structure -
+        For using this script successfully, you should have the 
+        following repository structure:
+        |- data      # It contains data to be used as input
+        |- docs      # It contains documentation
+        |- results   # Output files will be created in this directory
+        \- src       # The script must be executed from this directory
+
+USAGE
+    python src/lineage_comparison
+
+ARGUMENTS
+                        
+VARIABLES DICTIONARY
+    
+
+SEE ALSO
+    Esearch
+    Elink
+
+'''
+
+# Import libraries
+
+# Register an email account
+
+# Take the file with the article IDs as the input file
+
+# Save the abstract of the articles
+
+# Save the IDs of other articles that cite it
+
+# Write a new file with the results 
+
+# Let the user know that the output file has been created
