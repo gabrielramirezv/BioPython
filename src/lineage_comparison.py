221ff9598e1100665318c520285376ec5345dbb1
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2024-08-23 20:31:25 -0600

    Algorithm to compare lineages of two different species is created

---
+++ src/lineage_comparison.py
@@ -0,0 +1,61 @@
+'''
+NAME
+    lineage_comparison
+
+VERSION
+    0.1
+
+AUTHOR
+    Gabriel Ramirez Vilchis
+
+GITHUB
+    https://github.com/gabrielramirezv/BioPython/blob/master/src/lineage_comparison.py
+
+DESCRIPTION
+    Search taxonomy files for two different species and compare their 
+    lineages
+
+CATEGORY
+    Entrez
+    
+REQUIREMENTS
+    - Software - 
+        You must have Python and BioPython installed
+    - Repository structure -
+        For using this script successfully, you should have the 
+        following repository structure:
+        |- data      # It contains data to be used as input
+        |- docs      # It contains documentation
+        |- results   # Output files will be created in this directory
+        \- src       # The script must be executed from this directory
+
+USAGE
+    python src/lineage_comparison
+
+ARGUMENTS
+                        
+VARIABLES DICTIONARY
+    
+
+SEE ALSO
+    Esearch
+
+'''
+
+# Import libraries
+
+# Register an email account
+
+# Search the information for Notoryctes typhlops
+
+# Get taxonomy file for Notoryctes typhlops
+
+# Search the information for Chrysocloris asiatica
+
+# Get taxonomy file for Chrysocloris asiatica
+
+# Get lineages of both organisms
+
+# Compare lineages
+
+# Report results to user
