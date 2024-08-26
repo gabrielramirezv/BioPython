0f8ba90624522d088af58c7be9ac6ef6f577ef95
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2024-08-26 16:21:58 -0600

    Algorithm to get the UniProt accession is created

---
+++ src/Uniprot_accession.py
@@ -0,0 +1,60 @@
+'''
+NAME
+    Uniprot_accession
+
+VERSION
+    0.1
+
+AUTHOR
+    Gabriel Ramirez Vilchis
+
+GITHUB
+    https://github.com/gabrielramirezv/BioPython/blob/master/src/Uniprot_accession.py
+
+DESCRIPTION
+    Receives a file with IDs of articles of interest and creates a file
+    with the abstracts of those articles, including IDs of other 
+    papers that cited them.
+
+CATEGORY
+    ExPASy
+    
+REQUIREMENTS
+    - Software - 
+        You must have Python and BioPython installed
+    - Repository structure -
+        For using this script successfully, you should have the 
+        following repository structure:
+        |- data      # It contains data to be used as input
+        |- docs      # It contains documentation
+        |- results   # Output files will be created in this directory
+        \- src       # The script must be executed from this directory
+
+USAGE
+    python src/Uniprot_accession.py
+
+ARGUMENTS
+                        
+VARIABLES DICTIONARY
+    
+
+SEE ALSO
+    Esearch
+
+'''
+
+# Import libraries
+
+# Register an email
+
+# Search in the protein database
+
+# Read the results
+
+# Get ID
+
+# Extract a GenBank from the protein database
+
+# Read the results
+
+# Get the protein information from other databases
