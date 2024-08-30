213a0f136f8432d1d7a15f0aa9cbc3b4665dd26b
Gabriel Ramírez Vilchis <100377756+gabrielramirezv@users.noreply.github.com>
2024-08-29 19:50:05 -0600

    Scripts for BLAST and getting information from Uniprot uploaded

---
+++ src/Uniprot_info.py
@@ -0,0 +1,127 @@
+'''
+NAME
+    Uniprot_accession
+
+VERSION
+    3.0
+
+AUTHOR
+    Gabriel Ramirez Vilchis
+
+GITHUB
+    https://github.com/gabrielramirezv/BioPython/blob/master/src/Uniprot_accession.py
+
+DESCRIPTION
+    Searches a specific protein in different databases, and it returns 
+    the IDs and accessions.
+
+CATEGORY
+    ExPASy
+    
+REQUIREMENTS
+    - Software - 
+        You must have Python and BioPython installed
+    - Repository structure -
+        For using this script successfully, you should have the 
+        following repository structure:
+        |- data      # It contains data to be used as input
+        |- docs      # It contains documentation
+        |- results   # Output files will be created in this directory
+        \- src       # The script must be executed from this directory
+
+USAGE
+    python src/Uniprot_accession.py
+
+ARGUMENTS
+    
+
+SEE ALSO
+    Esearch
+
+'''
+
+# Import libraries
+import os
+import argparse
+from Bio import Entrez, SeqIO, ExPASy, SwissProt
+
+# Create parser
+parser = argparse.ArgumentParser(description="Search taxonomy files " 
+                                 + "for two different species and " 
+                                 + "compare their lineages")
+
+# Store the arguments
+parser.add_argument("-e", "--email",
+                    help="Email to use in Entrez",
+                    type=str,
+                    required=False,
+                    default="gramirez@lcg.unam.mx")
+
+parser.add_argument("-t", "--term",
+                    help="Term to search",
+                    type=str,
+                    required=False,
+                    default="DEFA[gene] AND Aedes aegypti[Orgn]")
+
+args = parser.parse_args()
+email = args.email
+term_to_search = args.term
+
+# Register an email
+Entrez.email = email
+
+# Search in the protein database
+with Entrez.esearch(db="protein", 
+                   term=term_to_search) as handle:
+    # Read the results
+    record = Entrez.read(handle)
+
+# Get ID
+prot_id = record["IdList"]
+
+# Extract a GenBank from the protein database
+with Entrez.efetch(db="protein", 
+                   id=prot_id, 
+                   rettype="gb", 
+                   retmode="text") as handle:
+    # Read the results
+    record = SeqIO.parse(handle, "genbank")
+
+    for protein in record:
+        if "UniProtKB" in protein.annotations['db_source']:
+            defa_prot = protein.annotations['accessions']
+
+# Create the files with the results and inform the user
+try:
+    if defa_prot:
+        print("\nThe following files have been created in the " 
+              + "directory results/\n")
+        for accession in defa_prot:
+            with open(f"results/{accession}.txt",
+                       'w') as accession_file:
+                handle = ExPASy.get_sprot_raw(accession)
+                accession_file.write(handle.read())
+            print(f"{accession}.txt")
+    else:
+        print("\nNo results found.\n")
+        
+# If there is not a results directory, create it
+except OSError:
+    os.mkdir("results")
+    if defa_prot:
+        for accession in defa_prot:
+            with open(f"results/{accession}.txt",
+                       'w') as accession_file:
+                handle = ExPASy.get_sprot_raw(accession)
+                accession_file.write(handle.read())
+            print(f"{accession}.txt")
+if defa_prot:
+    for accession in defa_prot:
+        with open(f"results/{accession}.txt") as handle:
+            record = SwissProt.read(handle)
+        print(f"\n\n Accession: {accession}\n",
+              f"Name: {record.entry_name}\n",
+              f"Created: {record.created}\n",
+              f"Annotation update: {record.annotation_update}\n",
+              f"Taxonomy ID: {record.taxonomy_id}\n")
+
--- src/blast.py
+++ src/blast.py
@@ -3,7 +3,7 @@ NAME
     blast
 
 VERSION
-    1.0
+    2.0
     
 TYPE
     Homework
@@ -16,7 +16,7 @@ GITHUB
 
 DESCRIPTION
     Receives a FASTA file and excute BLAST online to get the 
-    alignments with the smallest pvalue.
+    alignments with the smallest evalue.
 
 CATEGORY
     BLAST
@@ -33,43 +33,140 @@ REQUIREMENTS
         \- src       # The script must be executed from this directory
 
 USAGE
-    python src/blast.py
+    From the workspace main place with the structure described above 
+    in front of you:
+        python src/blast.py [-h] -i INPUTFILE [-f FORMAT] 
+            [-b BLASTTYPE] [-d DATABASE] [-o OUTPUTFILE] 
+            [-e EVALUE]
 
 ARGUMENTS
-    None
+    -h, --help              Show this help message and exit
+    -i INPUTFILE, --inputfile INPUTFILE
+                            Path to the file with the sequence to blast
+    -f FORMAT, --format FORMAT
+                            Format of the sequence file
+    -b BLASTTYPE, --blasttype BLASTTYPE
+                            Blast type to be executed
+    -d DATABASE, --database DATABASE
+                            Database to blast
+    -o OUTPUTFILE, --outputfile OUTPUTFILE
+                            Path to the file to create with the results
+    -e EVALUE, --evalue EVALUE
+                            e value limit for the alignments
                         
 VARIABLES DICTIONARY
+    args: Arguments received from the user
+    blast_type: Type of blast to execute
+    blast_xml: XML result of the BLAST
+    blst_record: Content of the BLAST XML after being parsed
+    data_base: Data base of BLAST to be aligned by the query sequence
+    E_VALUE_THRESH: e value limit for the alignments
+    file_format: Format of the input file
+    number_of_alignment: Count of the alignments being reported
+    output_file: Path to the file to create with the results
+    parser: Arguments parser
+    sequence: Sequence after being read from the input file
+    sequence_file: Path to the file with the sequence to blast
     
-
 SEE ALSO
     blastn
 
 '''
 
 # Import libraries
+import argparse
 from Bio import SeqIO
 from Bio.Blast import NCBIWWW, NCBIXML
 
-# Get the sequence
-sequence = SeqIO.read("data/opuntia1.fasta", format="fasta")
+# Define function to print the results or write them in a file
+def print_results(number_of_alignment, alignment, e_value):
+    '''
+    It writes each alignment BLAST results in a document or print them
+    to sreen, according to the user's choice.
+        Parameters:
+            number of alignment (int): Number of the alignment to be 
+                written
+            alignment (obj): Alignment to be reported
+            hsp.expect (float): e value of the alignment
+    '''
+    if output_file:
+        with open(output_file, 'w') as output:
+            output.write(f"***Alignment {number_of_alignment}***"
+                         + f"\nsequence: {alignment.title}"
+                         + f"\nlength: {alignment.length}"
+                         + f"\ne value: {e_value}")
+    else:
+        print(f"***Alignment {number_of_alignment}***\n"
+              "\nsequence:" , alignment.title,
+              "\nlength: ", alignment.length,
+              "\ne value: ", e_value)
+
+# Create parser
+parser = argparse.ArgumentParser(description="Receives a FASTA file "
+                                 + "and execute BLAST online to get " 
+                                 + "the alignments with the smallest " 
+                                 + "pvalue.")
+
+# Define the arguments
+parser.add_argument("-i", "--inputfile",
+                    help="Path to the file with the sequence to blast",
+                    type=str,
+                    required=True)
+
+parser.add_argument("-f", "--format",
+                    help="Format of the sequence file",
+                    type=str,
+                    required=False,
+                    default="fasta")
+
+parser.add_argument("-b", "--blasttype",
+                    help="Blast type to be executed",
+                    type=str,
+                    required=False,
+                    default="blastn")
+
+parser.add_argument("-d", "--database",
+                    help="Database to blast",
+                    type=str,
+                    required=False,
+                    default="nr")
+
+parser.add_argument("-o", "--outputfile",
+                    help="Path to the file to create with the results",
+                    type=str,
+                    required=False)
+
+parser.add_argument("-e", "--evalue",
+                    help="e value limit for the alignments",
+                    type=float,
+                    required=False,
+                    default=0.05)
+
+# Store the arguments
+args = parser.parse_args()
+sequence_file = args.inputfile
+file_format  = args.format
+output_file = args.outputfile
+blast_type = args.blasttype
+data_base = args.database
+E_VALUE_THRESH = args.evalue
+
+# Read the sequence from the input file
+sequence = SeqIO.read(sequence_file, format=file_format)
 
 # Execute BLAST
-blast_xml = NCBIWWW.qblast("blastn", "nr", sequence.seq)
+blast_xml = NCBIWWW.qblast(blast_type, data_base, sequence.seq)
 
 # Get the information from the xml file
 blast_record = NCBIXML.read(blast_xml)
 
-# Get the alignments with pvalue < 0.05 and print them
-E_VALUE_THRESH = 0.05
+# Get the alignments with evalue < 0.05 and print them
+number_of_alignment = 0
 for alignment in blast_record.alignments:
     for hsp in alignment.hsps:
         if hsp.expect < E_VALUE_THRESH:
-            print("****Alignment****")
-            print("sequence:", alignment.title)
-            print("length:", alignment.length)
-            print("e value:", hsp.expect)
-            print("score:",hsp.score)
-            print(hsp.query[0:75] + "...")
-            print(hsp.match[0:75] + "...")
-            print(hsp.sbjct[0:75] + "...")
+            number_of_alignment += 1
+            print_results(number_of_alignment, alignment, hsp.expect)
+            
+# Let the user know that the process is finished
 print("\nDone.")
